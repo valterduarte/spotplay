3c41afb2a4a1e058d187caeff8e91edc986ee7e3
Valter Duarte <valter.rduarte@gmail.com>
2023-05-23 17:31:44 -0300

    feature: created a button component

---
+++ src/components/greenButton/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function GreenButton({ label ='' }) {
+  return (
+    <button className="btn-register">
+      {label}
+    </button>
+
+    
+  )
+}
+
+export default GreenButton
