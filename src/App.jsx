24dd8295d5644632f1f10e3dbd9f4d628cf4b764
Valter Duarte <valter.rduarte@gmail.com>
2023-05-23 17:24:51 -0300

    feat:add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/home"
+import Signin from "./pages/signin"
+import Register from "./pages/register"
+
 
 import './global.css'
 
 
 function App() {  
   return (
-    <Home />
+   <Router>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+   </Router>
   )
 }
 
