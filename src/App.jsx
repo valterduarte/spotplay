08cdae9640c8f2d2761c3c8e72f747668dc2bd7d
Valter Duarte <valter.rduarte@gmail.com>
2023-06-21 17:33:56 -0300

    chore

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/home"
 import Signin from "./pages/signin"
 import Register from "./pages/register"
-
+import Teste from "./pages/teste"
 
 import './global.css'
 
-
-function App() {  
+export default function App() {  
   return (
    <Router>
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/teste" element={<Teste />} />
     </Routes>
    </Router>
   )
 }
 
-export default App
