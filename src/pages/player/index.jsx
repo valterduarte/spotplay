d8c32081f4c52ba9e06854336cc0461cf5becf20
Valter Duarte <valter.rduarte@gmail.com>
2025-02-05 14:48:41 -0300

    feat:conect spotfy APY

--- src/pages/player/index.jsx
+++ src/pages/player/index.jsx
@@ -2,28 +2,67 @@ import spotfy from "../../assets/imgs/logo.png"
 import './style.css'
 import Carrosselsongs from "../../components/Carrosselsongs"
 import Songslist from '../../components/Songslist'
+import React, { useEffect, useState } from 'react';
 
 function Player() {
+  const [token, setToken] = useState('');
+  const [tracks, setTracks] = useState([]);
+
+    // esse useefect peag os dados
+  useEffect(() => {
+    async function fetchToken() {
+      const clientId = "68e57a12f2bd44f48da49c4cfbd01c25";
+      const clientSecret = "e327edb3ff2c4b528a807b1f7f060858";
+
+      const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
+      }).catch(err => console.error("Error:", err));
+
+      const data = await response.json();
+      console.log(data.access_token)
+      setToken(data.access_token);
+    }
+
+    fetchToken()
+  }, []);
+
+  // esse useefect peag as musicas
+  useEffect(() => {
+    async function getTracks() {
+      const response = await fetch("https://api.spotify.com/v1/search?q=artist%3ASabrina%20Carpenter&type=track", {
+        method: "GET",
+        headers: { "Authorization": `Bearer ${token}`},
+      }).catch(err => console.error("Error:", err));
+
+      const data = await response.json();
+      
+      console.log(data.tracks.items)
+      setTracks(data.tracks.items);
+    }
+
+    if (token !== '') getTracks();
+  }, [token]);
+  
   return (
-      <div className="container" id="player">
-        <div className="wrapper-logo-menu">
-          <img id="player-logo-spotfy" src={spotfy} alt="Logo spotfy" /> 
-          <nav>...</nav>
-        </div>
+    <div className="container" id="player">
+      <div className="wrapper-logo-menu">
+        <img id="player-logo-spotfy" src={spotfy} alt="Logo spotfy" /> 
+        <nav>...</nav>
+      </div>
 
-        <h3>News</h3>
-        <div id="songbox">
-          <Carrosselsongs/>
-          <Carrosselsongs/>
-          <Carrosselsongs/>
-        </div>
+      <h3>News</h3>
+      <div id="songbox">
+        {tracks.slice(0, 3).map((track) => <Carrosselsongs key={track.id} />)}
+          
+      </div>
 
-        <h3>Playlist</h3>
-        <div id="playlist">
-          <Songslist/>
-          <Songslist/>
-        </div>
-      </div> 
+      <h3>Playlist</h3>
+      <div id="playlist">
+      {tracks.slice(0, 3).map((song) => <Songslist key={song.id} />)}
+      </div>
+    </div> 
   )
 }
 
